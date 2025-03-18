4ea67d61df47ec01ad32b14525f2ec5b7a4965c4
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-03-18 22:11:23 +0100

    add identity to project

--- Program.cs
+++ Program.cs
@@ -1,11 +1,20 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using OresAndCores_2.Data;
+using Microsoft.AspNetCore.Identity;
+using OresAndCores_2.Areas.Identity.Data;
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("ApplicationDbContextConnection")));
+
 builder.Services.AddDbContext<OresAndCores_2Context>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("OresAndCores_2Context") ?? throw new InvalidOperationException("Connection string 'OresAndCores_2Context' not found.")));
 
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true).AddEntityFrameworkStores<ApplicationDbContext>();
+
 // Add services to the container.
+builder.Services.AddRazorPages();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
@@ -30,5 +39,6 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
+app.MapRazorPages();
 
 app.Run();
