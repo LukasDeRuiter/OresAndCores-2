66cd8da63815e5a9a9f35e9c4769acb74e782c5c
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-03-28 15:28:54 +0100

    update readme and migrations

---
+++ Migrations/20250328135845_EnvironmentObjects.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace OresAndCores_2.Migrations
+{
+    /// <inheritdoc />
+    public partial class EnvironmentObjects : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "EnvironmentObject",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Path = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Items = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EnvironmentObject", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "EnvironmentObject",
+                columns: new[] { "Id", "Items", "Name", "Path" },
+                values: new object[,]
+                {
+                    { 1, "test", "rock", "assets/mine/sprites/objects/rock.png" },
+                    { 2, "test", "copper-rock", "assets/mine/sprites/objects/copper-rock.png" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EnvironmentObject");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 Add scaffolding:
-dotnet aspnet-codegenerator controller -name InventoryController -m Inventory -dc MvcInventory.Data.MvcInventoryContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries --databaseProvider sqlite
+dotnet aspnet-codegenerator controller -name EnvironmentObjectController -m EnvironmentObject -dc EnvironmentObject.Data.EnvironmentObjectContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries --databaseProvider sqlite
 
 create migration:
 dotnet ef migrations add {YourMigrationName} --context OresAndCores_2Context
