d255fea34bafea573b69797fa2481a996156e646
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-03-28 15:28:35 +0100

    add object context

--- Data/OresAndCores_2Context.cs
+++ Data/OresAndCores_2Context.cs
@@ -15,7 +15,17 @@ namespace OresAndCores_2.Data
         }
 
         public DbSet<OresAndCores_2.Models.Ore> Ore { get; set; } = default!;
-
         public DbSet<OresAndCores_2.Models.Inventory> Inventory { get; set; } = default!;
+        public DbSet<OresAndCores_2.Models.EnvironmentObject> EnvironmentObject { get; set; } = default!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<EnvironmentObject>().HasData(
+                new EnvironmentObject { Id = 1, Name = "rock", Path = "assets/mine/sprites/objects/rock.png", Items = "test" },
+                new EnvironmentObject { Id = 2, Name = "copper-rock", Path = "assets/mine/sprites/objects/copper-rock.png", Items = "test" }
+            );
+        }
     }
 }
