090cdeec37865f244ab2e2927264a2b20a30183c
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-03-28 15:55:14 +0100

    add layout change and migrations

---
+++ Migrations/20250328144356_AddItems.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OresAndCores_2.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddItems : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Item",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Path = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Value = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Item", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Item",
+                columns: new[] { "Id", "Name", "Path", "Value" },
+                values: new object[] { 1, "stone-item", "assets/mine/sprites/items/stone-item.png", 10 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Item");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 
 Add scaffolding:
-dotnet aspnet-codegenerator controller -name EnvironmentObjectController -m EnvironmentObject -dc EnvironmentObject.Data.EnvironmentObjectContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries --databaseProvider sqlite
+dotnet aspnet-codegenerator controller -name ItemController -m Item -dc Item.Data.ItemContext --relativeFolderPath Controllers --useDefaultLayout --referenceScriptLibraries --databaseProvider sqlite
 
 create migration:
 dotnet ef migrations add {YourMigrationName} --context OresAndCores_2Context
