cf4e0af51543527e37b4f49136c58a91b8455bae
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-03-30 20:57:04 +0200

    change seeder values

--- Data/OresAndCores_2Context.cs
+++ Data/OresAndCores_2Context.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using OresAndCores_2.Models;
@@ -24,12 +25,32 @@ namespace OresAndCores_2.Data
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.Entity<EnvironmentObject>().HasData(
-                new EnvironmentObject { Id = 1, Name = "rock", Path = "assets/mine/sprites/objects/rock.png", Items = "test" },
-                new EnvironmentObject { Id = 2, Name = "copper-rock", Path = "assets/mine/sprites/objects/copper-rock.png", Items = "test" }
+                new EnvironmentObject 
+                { 
+                    Id = 1,
+                    Name = "rock", 
+                    Path = "assets/mine/sprites/objects/rock.png", 
+                    Items = JsonSerializer.Serialize(new List<ObjectItem>
+                    {
+                        new ObjectItem {ItemId = 1, Amount = 2, DropChance = 100}
+                    }),
+                },
+                new EnvironmentObject 
+                { 
+                    Id = 2, 
+                    Name = "copper-rock",
+                    Path = "assets/mine/sprites/objects/copper-rock.png", 
+                    Items = JsonSerializer.Serialize(new List<ObjectItem>
+                    {
+                        new ObjectItem {ItemId = 1, Amount = 1, DropChance = 100},
+                        new ObjectItem {ItemId = 2, Amount = 2, DropChance = 100},
+                    }),
+                }
             );
 
             modelBuilder.Entity<Item>().HasData(
-                new Item { Id = 1, Name = "stone-item", Path = "assets/mine/sprites/items/stone-item.png", Value = 10 }
+                new Item { Id = 1, Name = "stone-item", Path = "assets/mine/sprites/items/stone-item.png", Value = 10 },
+                new Item { Id = 2, Name = "copper-ore-item", Path = "assets/mine/sprites/items/copper-ore-item.png", Value = 50 }
             );
         }
     }
---
+++ Migrations/20250330173438_newValues.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OresAndCores_2.Migrations
+{
+    /// <inheritdoc />
+    public partial class newValues : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "EnvironmentObject",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Items",
+                value: "[{\"ItemId\":1,\"Amount\":2,\"DropChance\":100}]");
+
+            migrationBuilder.UpdateData(
+                table: "EnvironmentObject",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Items",
+                value: "[{\"ItemId\":1,\"Amount\":1,\"DropChance\":100},{\"ItemId\":2,\"Amount\":2,\"DropChance\":100}]");
+
+            migrationBuilder.InsertData(
+                table: "Item",
+                columns: new[] { "Id", "Name", "Path", "Value" },
+                values: new object[] { 2, "copper-ore-item", "assets/mine/sprites/items/copper-ore-item.png", 50 });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Item",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.UpdateData(
+                table: "EnvironmentObject",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Items",
+                value: "test");
+
+            migrationBuilder.UpdateData(
+                table: "EnvironmentObject",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "Items",
+                value: "test");
+        }
+    }
+}
