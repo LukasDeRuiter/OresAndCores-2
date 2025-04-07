178f9c91bfee3ae46f3eb4bcf991e4c66e6c1267
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-04-07 13:21:40 +0200

    add enemy model

---
+++ Models/Enemy.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json;
+using Microsoft.AspNetCore.Identity;
+
+namespace OresAndCores_2.Models;
+
+public class Enemy {
+
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string Name { get; set; }
+
+    [Required]
+    public string Sprite { get; set; }
+
+    [Required]
+    public int Health { get; set; }
+
+    [Required]
+    public int Speed { get; set; }
+
+    [Required]
+     public string Items { get; set; }
+
+     public List<ObjectItem> GetItems()
+     {
+        return string.IsNullOrEmpty(Items) ? new List<ObjectItem>() : JsonSerializer.Deserialize<List<ObjectItem>>(Items);
+     }
+}
