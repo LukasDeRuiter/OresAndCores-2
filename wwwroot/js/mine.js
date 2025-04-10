865239b6ac41a64cc4f4b28f0ef2a5b10c3e6492
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-04-10 12:31:08 +0200

    remove old entities file

--- wwwroot/js/mine.js
+++ wwwroot/js/mine.js
@@ -27,24 +27,3 @@ var config = {
 };
 
 var game = new Phaser.Game(config);
-
-function saveInventoryToServer(inventory) {
-    return fetch('api/mineApi/save', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(inventory)
-    })
-    .then(response => {
-        return response.json();
-    })
-    .then(data => {
-        console.log("Server Response:", data);
-        return data;
-    })
-    .catch(error => {
-        console.error("Error:", error);
-        return error;
-    });
-}
