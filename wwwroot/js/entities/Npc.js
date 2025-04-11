e2fd37b12aa558f4fba6b15c05244dd5eeea667e
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-04-11 12:12:54 +0200

    add merchant sprite and npc class

---
+++ wwwroot/js/entities/Npc.js
@@ -0,0 +1,35 @@
+export class Npc extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, name, spritesheet) {
+        super(scene, x, y, spritesheet);
+
+        this.scene = scene;
+        this.name = name;
+        this.spritesheet = spritesheet;
+        this.idleAnimation = `${name}-idle`;
+
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+
+        this.setOrigin(0.5);
+        this.setDepth(30);
+        this.setScale(1);
+        this.setCollideWorldBounds(true);
+
+        this.createAnimations(scene, name);
+
+        this.play(this.idleAnimation);
+    }
+
+    createAnimations(scene) {
+        if (!scene.anims.exists(this.idleAnimation)) {
+            scene.anims.create({
+                key: this.idleAnimation,
+                frames: scene.anims.generateFrameNumbers(this.name, {
+                    frames: [0, 1, 2, ]
+                }),
+                frameRate: 6,
+                repeat: -1
+            });
+        }
+    }
+}
