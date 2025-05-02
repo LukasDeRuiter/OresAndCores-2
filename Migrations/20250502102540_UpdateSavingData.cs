581159ee9ede89868f07a8f299277d5ac0c30bbe
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-05-02 12:36:42 +0200

    add migrations for new inventory saving fields

---
+++ Migrations/20250502102540_UpdateSavingData.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace OresAndCores_2.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateSavingData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PlayerLevel",
+                table: "Inventory",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "PlayerMoney",
+                table: "Inventory",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ToolData",
+                table: "Inventory",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PlayerLevel",
+                table: "Inventory");
+
+            migrationBuilder.DropColumn(
+                name: "PlayerMoney",
+                table: "Inventory");
+
+            migrationBuilder.DropColumn(
+                name: "ToolData",
+                table: "Inventory");
+        }
+    }
+}
