bf75ef407b7f92beacd302aa0cc69cbc52baed13
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-05-03 23:53:08 +0200

    add ui to menu

--- wwwroot/js/ui/Menu.js
+++ wwwroot/js/ui/Menu.js
@@ -1,9 +1,26 @@
 export class Menu {
     constructor(scene) {
         this.scene = scene;
+        this.isVisible = false;
+
+        this.ui = this.createUI();
     }
 
     toggleMenu() {
         
+    }
+
+    createUI() {
+        if (this.ui) {
+            this.ui.destroy();
+            this.ui = null;
+
+            return;
+        }
+
+        this.ui = this.scene.add.container(400, 300 , []);
+        this.ui.setDepth(1000);
+        this.ui.setScrollFactor(0);
+        this.ui.setVisible(this.isVisible);
     }
 }
