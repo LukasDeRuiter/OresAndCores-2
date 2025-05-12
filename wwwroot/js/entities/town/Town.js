803c3278d09a3218cbc9d6b0d81a4fa0dc344078
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-05-12 13:40:03 +0200

    add town level option

--- wwwroot/js/entities/town/Town.js
+++ wwwroot/js/entities/town/Town.js
@@ -2,15 +2,14 @@ export class Town {
     constructor(scene, level) {
         this.scene = scene;
         this.level = level;
-
-        this.scene.registry.set("townLevel", this.level);
     }
 
     upgrade(entity) {
         if (entity === 'town') {
             this.level += 1;
 
-             this.scene.transitionSaver.transition("SceneTown", "Upgrading...", this.scene.player.inventory, this.scene.player.level);
+            this.scene.registry.set("townLevel", this.level);
+            this.scene.transitionSaver.transition("SceneTown", "Upgrading...", this.scene.player.inventory, this.scene.player.level);
         }
     }
 }
