77c3a09a826eed0ce368e7c0f9cdc447a195d2ea
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-05-13 12:34:48 +0200

    add controls menu

--- wwwroot/js/ui/Menu.js
+++ wwwroot/js/ui/Menu.js
@@ -34,10 +34,11 @@ export class Menu {
 
         const background = this.scene.add.rectangle(0, 0, 150, 250, 0X000000, 0.3);
         const resumeButton = this.createButton("Resume", 0, this.toggleMenu.bind(this));
-        const quitButton = this.createButton("Quit", 30, this.quitGame.bind(this));
+        const controlsButton = this.createButton("Controls", 30, this.showControls.bind(this));
+        const quitButton = this.createButton("Quit", 60, this.quitGame.bind(this));
 
-        this.ui = this.scene.add.container(400, 300 , [background, resumeButton, quitButton]);
-        this.ui.setDepth(1000);
+        this.ui = this.scene.add.container(400, 300 , [background, resumeButton, controlsButton, quitButton]);
+        this.ui.setDepth(1000); 
         this.ui.setScrollFactor(0);
         this.ui.setVisible(this.isVisible);
     }
@@ -66,9 +67,42 @@ export class Menu {
         );
 
         return button;
-    }
+    } 
 
     updateUI(){
         this.ui.setVisible(this.isVisible);
+        if (this.controlsUI && this.isVisible === false) {
+            this.controlsUI.setVisible(this.isVisible);
+        }
+    }
+
+    showControls() {
+        this.ui.setVisible(false);
+
+        if (this.controlsUI) {
+            this.controlsUI.setVisible(true);
+            return;
+        }
+
+        const background = this.scene.add.rectangle(0, 0, 200, 200, 0X000000, 0.3);
+        const controlsText = this.scene.add.text(-90, -80, 
+        "Controls:\n\n" +
+        "ESC - Main menu\n" +
+        "WASD - Move\n" +
+        "R - Use tool\n" +
+        "E - Open Inventory\n" +
+        "Left mouse - Interact with objects\n" +
+        "1, 2, 3 - Select tool\n", 
+            { fontSize: "12px", fill: "#fff", wordWrap: { width: 180 } }
+        );
+
+        const backButton = this.createButton("Back", 180, () => {
+            this.controlsUI.setVisible(false);
+            this.ui.setVisible(true);
+        });
+
+        this.controlsUI = this.scene.add.container(400, 300, [background, controlsText, backButton]);
+        this.controlsUI.setDepth(1001);
+        this.controlsUI.setScrollFactor(0);
     }
 }
