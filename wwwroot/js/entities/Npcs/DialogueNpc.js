e09aef83aa2dec33bc45c18f2966225bdf27725e
Lukas de Ruiter <lukas_kremlin@hotmail.com>
2025-05-26 21:46:55 +0200

    add constructor

--- wwwroot/js/entities/Npcs/DialogueNpc.js
+++ wwwroot/js/entities/Npcs/DialogueNpc.js
@@ -1,5 +1,9 @@
 import { Npc } from "./Npc.js";
 
 export class Npc extends Npc {
+    constructor(scene, x, y, name, spritesheet, dialogue) {
+        super(scene, x, y, name, spritesheet);
 
+        this.dialogue = dialogue;
+    }
 }
